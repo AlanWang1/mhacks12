293a86368fafafb22f9825de70f64d4ebbf419b0
Alan Wang <alan.wang9018@gmail.com>
2019-10-12 11:50:30 -0400

    fixed booboo (epic style)

--- src/components/Analytics.js
+++ src/components/Analytics.js
@@ -3,50 +3,7 @@ import QSet from './QSet';
 class Analytics extends React.Component {
     render() {
         return (
-                <section className="container">
-                    <section className="columns is-centered">
-                    <div className="column is-three-fifths">
-
-                    <div className="columns is-multiline">
-                        {[
-                            {
-                                title: 'Stoichiometery',
-                                description:'Cool',
-                                questions: ['', '', '', '', '', '']
-                            }, {
-                                title: 'Energetics',
-                                description:'Good',
-                                questions: ['', '', '', '', '']
-                            }, {
-                                title: 'Atomic Structure',
-                                description:'Good',
-                                questions: ['', '', '',  '']
-                            }, {
-                                title: 'Biochemistry',
-                                description:'Good',
-                                questions: ['', '']
-                            }
-                        ].map(set => 
-                        <div key={set.title} className="column is-12">
-                            <QSet title={set.title} description={set.description} numQuestions={set.questions.length}/>
-
-                        </div>)}
-
-                        {/* ['Cool', 'Good', 'Good', 'Good'].map(subject => 
-                        <div key={subject} className="column is-narrow">
-                            <QSet description={subject}/>
-
-                        </div>) */}
-
-                    
-                    </div>
-
-                    </div>
-
-                    </section>
-                    
-                </section>
-            
+                "d"
             );
     }
 }
--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,9 +1,47 @@
 import React from 'react';
-
+import QSet from './QSet';
 class ToDo extends React.Component {
     render() {
         return (
-               "d"
+            <section className="container">
+            <section className="columns is-centered">
+            <div className="column is-three-fifths">
+
+            <div className="columns is-multiline">
+                {[
+                    {
+                        title: 'Stoichiometery',
+                        description:'Cool',
+                        questions: ['', '', '', '', '', '']
+                    }, {
+                        title: 'Energetics',
+                        description:'Good',
+                        questions: ['', '', '', '', '']
+                    }, {
+                        title: 'Atomic Structure',
+                        description:'Good',
+                        questions: ['', '', '',  '']
+                    }, {
+                        title: 'Biochemistry',
+                        description:'Good',
+                        questions: ['', '']
+                    }
+                ].map(set => 
+                <div key={set.title} className="column is-12">
+                    <QSet title={set.title} description={set.description} numQuestions={set.questions.length}/>
+
+                </div>)}
+
+
+            
+            </div>
+
+            </div>
+
+            </section>
+            
+        </section>
+    
             );
     }
 }
