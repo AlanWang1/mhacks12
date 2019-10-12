47ed33ff82061a59c176a8fd0c102b1feb60a2e2
plin0009 <plin0009@gmail.com>
2019-10-12 19:31:32 -0400

    Changes to landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,35 +3,30 @@ import ReactDOM from 'react-dom';
 import Layout from '../components/Layout';
 ReactDOM.render(
     <Layout>
-        
         <section className="hero is-fullheight">
-     
-            
-            <section className="hero-body has-text-centered has-background-rgba(0, 0, 0, 0.7)">
+            <div className="hero-body has-text-centered">
                 <div className="container">
-                    <h1 className="title is-centered">
+                    <h1 className="title is-1">
                         TimeCheck
                     </h1>
                     <h2 className="subtitle">
-                        subtitle
+                        An unparalleled platform of student-teacher interaction for improving test results.
                     </h2>
               
-                    <a className="button is-large">
-                                Get Started
+                    <a className="button is-large is-primary" href="/signin">
+                        Get Started
                     </a>   
                 </div>
-            
-            </section>
+            </div>
           
-        <section className=" level has-text-centered">
-                  
+            <div className="hero-foot has-text-centered">
                     <a href="#scroll">
-                    <h2>
-                        Learn More
-                    </h2>
-                    </a>
+                        <h2>
+                            Learn More
+                        </h2>
 
-        </section> 
+                    </a>
+                </div> 
         </section>
 
         <section className="section" id="scroll">
@@ -44,7 +39,7 @@ ReactDOM.render(
                         <h1 className="title">
                             What is TimeCheck?
                         </h1>
-                        <p className> 
+                        <p> 
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
                         </p>
